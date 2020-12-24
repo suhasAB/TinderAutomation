d4cacb995f82d8281cbfc88a69228d56e42a1704
suhasAB <suhasbalagar@gmail.com>
2020-12-24 09:08:31 +0530

    Update tin.js

--- tin.js
+++ tin.js
@@ -1,9 +1,7 @@
-for(i=40;i>0;i--)
+for(i=100;i>0;i--)
 {
     setTimeout(() => {  
         document.getElementsByClassName("Pos(r) Z(1)")[4].click();
         console.log("done");
-                     }, 2000);
-    
-
+                     }, 5000);
 }
