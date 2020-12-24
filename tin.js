2fe6493b74308b76339de8413027bd47168d83fe
suhasAB <suhasbalagar@gmail.com>
2020-12-25 00:37:27 +0530

    Update tin.js

--- tin.js
+++ tin.js
@@ -1,7 +1,14 @@
-for(i=100;i>0;i--)
-{
-    setTimeout(() => {  
-        document.getElementsByClassName("Pos(r) Z(1)")[4].click();
-        console.log("done");
-                     }, 5000);
+var i = 1;                  //  set your counter to 1
+
+function myLoop() {         //  create a loop function
+  setTimeout(function() {   //  call a 0.7 s setTimeout when the loop is called
+    console.log('liked it');
+    document.getElementsByClassName("Pos(r) Z(1)")[4].click();   //  this code will click on like button
+    i++;                    //  increment the counter
+    if (i < 100) {           //  if the counter < 100, call the loop function-> the # of time you want to press like
+      myLoop();             //  ..  again which will trigger another
+    }                       //  ..  setTimeout()
+  }, 700)
 }
+
+myLoop();
